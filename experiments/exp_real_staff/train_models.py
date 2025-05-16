d9bb3e8ec0b7926e21b93b60f27ab0fef2287660
Felix Grabner <134441547+gfelix1902@users.noreply.github.com>
2025-05-16 22:57:46 +0200

    Update train_models.py

--- experiments/exp_real_staff/train_models.py
+++ experiments/exp_real_staff/train_models.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
 from experiments.model_training import train_models
 import utils
 import joblib
